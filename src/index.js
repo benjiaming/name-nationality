0154f8db3287f123039169b39ee3d49195614668
Ben Blazke <ben.blazke@coursehero.com>
2024-04-25 11:52:52 -0700

    meticulous

--- src/index.js
+++ src/index.js
@@ -8,17 +8,18 @@ import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader'
 async function startApp() {
   if(process.env.NODE_ENV === 'development' ||  process.env.VERCEL_ENV === 'preview') {
     await tryLoadAndStartRecorder({
-     projectId: 'L9K2n9bouru3E81FuOa9Cg9BHXh2SkP1E39kIxEV',
-    });
+      projectId: "DHPKqhDlOctW8kHuOuFRkvymv2YXhIennj2zShO0",
+      isProduction: false,
+    })
   }
-  
+
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   );
-  
+
   // If you want to start measuring performance in your app, pass a function
   // to log results (for example: reportWebVitals(console.log))
   // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
